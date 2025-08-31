7b4bc7ce96c2897c6a3f0cc817e1af9cffb89931
Artem Kliukvin <aklyukvin@yandex.com>
2025-08-31 15:42:46 +0100

    fix

--- preprocessing/transform/combining_dataset.py
+++ preprocessing/transform/combining_dataset.py
@@ -2,19 +2,18 @@ import torch
 from torch.utils.data import IterableDataset
 from typing import Callable, Iterator, Iterable, TypeVar    
 
-T_in = TypeVar('T_in')
-T_out = TypeVar('T_out')
+T = TypeVar('T')
 
-class CombiningDataset(IterableDataset[T_in]):
+class CombiningDataset(IterableDataset[T]):
     def __init__(
         self,
-        datasets: Iterable[IterableDataset[T_in]],
-        op: Callable[..., T_out],
+        datasets: Iterable[IterableDataset],
+        op: Callable[..., T],
     ):
         self.datasets = list(datasets)
         self.op = op
 
-    def __iter__(self) -> Iterator[T_out]:
+    def __iter__(self) -> Iterator[T]:
         iterators = [iter(ds) for ds in self.datasets]
         for items in zip(*iterators):
             yield self.op(*items)
